e310e701c1427cd0671085a4145dd710ed3d3c6a
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-04-09 09:54:58 +0200

    add modle

---
+++ Stock_management_fast_api/database/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class StockSummary(Base):
+    __tablename__ = "stock_summary"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String,unique=True, index=True)
+    strength = Column(String, index=True)
+    weakness = Column(String, index=True)
