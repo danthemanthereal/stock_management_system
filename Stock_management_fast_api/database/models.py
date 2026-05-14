9405aee91838e848eace90e3c9bfa3250e30c423
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-05-14 23:00:22 +0200

    add rel between metric and category

--- Stock_management_fast_api/database/models.py
+++ Stock_management_fast_api/database/models.py
@@ -38,9 +38,10 @@ class FinancialMetric(Base):
     name = Column(String, unique=True, index=True)
     should_rise = Column(Boolean, index=False)
     reference_value = Column(Integer, index=False)
-    category = Column(String, index=False)
     unit = Column(String, index=False)
     is_active = Column(Boolean, index=False, default=True)
+    category_id = Column(Integer, ForeignKey("financial_metric_category.id"))
+    category_rel = relationship("FinancialMetricCategory", back_populates="metrics")
     branch_profiles = relationship(
         "FinancialMetricBranchProfile",
         secondary=branch_profile_metric_link,
@@ -58,3 +59,10 @@ class FinancialMetricBranchProfile(Base):
         secondary=branch_profile_metric_link,
         back_populates="branch_profiles",
     )
+
+class FinancialMetricCategory(Base):
+    __tablename__ = "financial_metric_category"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+
+    metrics = relationship("FinancialMetric", back_populates="category_rel")
