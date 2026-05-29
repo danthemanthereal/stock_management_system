835529d4d59096ce3abeb3faac7c9792ec00a7d2
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-05-29 11:02:12 +0200

    fi error

--- Stock_management_fast_api/src/ticker_stock_component/ticker_stock.py
+++ Stock_management_fast_api/src/ticker_stock_component/ticker_stock.py
@@ -11,17 +11,18 @@ class TickerStock:
 
 
     def get_ticker_of_a_stock(self, company_name):
+        try:
+            url = "https://financialmodelingprep.com/stable/search-name"
 
-        url = "https://financialmodelingprep.com/stable/search-name"
+            params = {
+                "query": company_name,
+                "apikey": os.getenv("FMP_API_KEY")
+            }
 
-        params = {
-            "query": company_name,
-            "apikey": os.getenv("FMP_API_KEY")
-        }
-
-        response = requests.get(url, params=params)
-        print("response in method ", response)
-        if response.status_code != 200:
-            raise Exception(f"API Error: {response.status_code}")
-        print("json response", response.json())
-        return response.json().get("symbol", "")
+            response = requests.get(url, params=params)
+            if response.status_code != 200:
+                raise Exception(f"API Error: {response.status_code}")
+            return response.json()[0].get("symbol", "")
+        except Exception as e:
+            print(e)
+            return ""
