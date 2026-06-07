a317c5caaa0a297a8eb2f5c33d59d3879e53becf
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-06-07 15:18:47 +0200

    have only metrics no raw data more

--- Stock_management_fast_api/src/financial_metric_calculator/financial_calculator.py
+++ Stock_management_fast_api/src/financial_metric_calculator/financial_calculator.py
@@ -1,12 +1,22 @@
+from src.financial_metric_calculator.utils import get_calculated_financial_metrics
+
+
 class FinancialMetricCalculator:
 
     def __init__(self, total_financial_metric_map):
         self.total_financial_metric_map = total_financial_metric_map
 
 
-    def get_calculated_financial_metric_map(self):
-        calculated_financial_metric_map = {}
+    async def get_calculated_financial_metric_map(self):
 
+        calculated_financial_metric_map = {}
+        financial_metrics_to_calculate = await get_calculated_financial_metrics()
+        for financial_metric in financial_metrics_to_calculate:
+            values = []
+            calculated_financial_metric_map[financial_metric] = values
 
         return calculated_financial_metric_map
+
+
+
 
