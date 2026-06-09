00990b01ea58b5d60adfd9e1166945dc35f5ba85
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-06-09 20:51:05 +0200

    add pfc

--- Stock_management_fast_api/src/financial_metric_calculator/financial_calculator.py
+++ Stock_management_fast_api/src/financial_metric_calculator/financial_calculator.py
@@ -126,7 +126,8 @@ class FinancialMetricCalculator:
             "operating_ratio":self.get_operating_ratio_last_four_years,
             "short_term_coverage_ratio":self.get_short_term_coverage_ratio_last_four_years,
             "earnings_per_share":self.get_earnings_per_share_last_four_years,
-            "book_value_per_share":self.get_book_value_per_share_last_four_years
+            "book_value_per_share":self.get_book_value_per_share_last_four_years,
+            "price_to_cashflow":self.get_price_to_cashflow_last_four_years
         }
         return metric_handlers
 
@@ -854,3 +855,6 @@ class FinancialMetricCalculator:
 
     def get_book_value_per_share_last_four_years(self):
         return self.total_financial_metric_map.get("book_value_per_share", [])
+
+    def get_price_to_cashflow_last_four_years(self):
+        return self.total_financial_metric_map.get("price_to_cashflow", [])
