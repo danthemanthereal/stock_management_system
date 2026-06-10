4aac33e59d036ebfc18acbca4c05d4a91ea7cb54
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-06-10 13:08:12 +0200

    add dynamic cal method of devide 2 things

--- Stock_management_fast_api/src/financial_metric_calculator/financial_calculator.py
+++ Stock_management_fast_api/src/financial_metric_calculator/financial_calculator.py
@@ -95,7 +95,7 @@ class FinancialMetricCalculator:
             "price_to_gf_value": partial(self.get_financial_metric_direct_by_map, "price_to_gf_value"),
             "rank_balancesheet": partial(self.get_financial_metric_direct_by_map, "rank_balancesheet"),
             "rank_gf_value": partial(self.get_financial_metric_direct_by_map, "rank_gf_value"),
-            "rank_growth": self.get_rank_growth_last_four_years,
+            "rank_growth": partial(self.get_financial_metric_direct_by_map, "rank_growth"),
             "rank_momentum": self.get_rank_momentum_last_four_years,
             "rank_predictability": self.get_rank_predictability_last_four_years,
             "rank_profitability": self.get_rank_profitability_last_four_years,
@@ -287,11 +287,6 @@ class FinancialMetricCalculator:
 
         return cash_burn_rate_last_four_years
 
-    def get_rank_gf_value_last_four_years(self):
-        return self.total_financial_metric_map.get("rank_gf_value", [])
-
-    def get_rank_growth_last_four_years(self):
-        return self.total_financial_metric_map.get("rank_growth", [])
 
     def get_rank_momentum_last_four_years(self):
         return self.total_financial_metric_map.get("rank_momentum", [])
