55b874d80069485b203a8db987168a566469696d
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-06-10 13:27:56 +0200

    add dynamic cal method of devide 2 things

--- Stock_management_fast_api/src/financial_metric_calculator/financial_calculator.py
+++ Stock_management_fast_api/src/financial_metric_calculator/financial_calculator.py
@@ -134,7 +134,9 @@ class FinancialMetricCalculator:
             "roi": partial(self.get_financial_metric_by_calculate_to_raw_date,
                                                                  self.total_financial_metric_map.get("Operating Income", []),
                                                                  self.total_financial_metric_map.get("Total Assets", [])),
-            "capital_turnover": self.get_capital_turnover_last_four_years,
+            "capital_turnover": partial(self.get_financial_metric_by_calculate_to_raw_date,
+                                                                 self.total_financial_metric_map.get("Revenue", []),
+                                                                 self.total_financial_metric_map.get("Total Assets", [])),
             "cash_per_share": self.get_cash_per_share_last_four_years,
             "ebitda_per_share": self.get_ebitda_per_share_last_four_years,
             "enterprise_value_to_ebit": self.get_enterprise_value_to_ebit_last_four_years,
@@ -311,16 +313,7 @@ class FinancialMetricCalculator:
 
 
 
-    def get_capital_turnover_last_four_years(self):
-        revenue_last_four_years = self.total_financial_metric_map.get("Revenue", [])
-        total_assets_last_four_years = self.total_financial_metric_map.get("Total Assets", [])
 
-        capital_turnover_last_four_years = []
-
-        for revenue,total_asset in zip(revenue_last_four_years, total_assets_last_four_years):
-            capital_turnover_last_four_years.append(round(float(revenue) / float(total_asset), 2))
-
-        return capital_turnover_last_four_years
 
     def get_cash_per_share_last_four_years(self):
         return self.total_financial_metric_map.get("cash_per_share", [])
