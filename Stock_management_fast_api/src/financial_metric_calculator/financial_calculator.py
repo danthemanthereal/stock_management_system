058229bad3f7552b3de86d426394b3bae1b332c6
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-06-10 13:33:43 +0200

    add dynamic cal method of devide 2 things

--- Stock_management_fast_api/src/financial_metric_calculator/financial_calculator.py
+++ Stock_management_fast_api/src/financial_metric_calculator/financial_calculator.py
@@ -145,7 +145,7 @@ class FinancialMetricCalculator:
             "enterprise_value_to_ocf": partial(self.get_financial_metric_direct_by_map, "enterprise_value_to_ocf"),
             "enterprise_value_to_revenue": partial(self.get_financial_metric_direct_by_map, "enterprise_value_to_revenue"),
             "free_cash_flow_per_share": partial(self.get_financial_metric_direct_by_map, "free_cash_flow_per_share"),
-            "growth_per_share_ebitda": self.get_growth_per_share_ebitda_last_four_years,
+            "growth_per_share_ebitda": partial(self.get_financial_metric_direct_by_map, "growth_per_share_ebitda"),
             "growth_per_share_eps": self.get_growth_per_share_eps_last_four_years,
             "growth_revenue_per_share": self.get_growth_revenue_per_share_last_four_years,
             "net_cash_per_share": self.get_net_cash_per_share_last_four_years,
@@ -311,8 +311,7 @@ class FinancialMetricCalculator:
             revenue_per_employee_last_four_years.append(round(float(revenue) / float(employee_amount), 2))
         return revenue_per_employee_last_four_years
 
-    def get_growth_per_share_ebitda_last_four_years(self):
-        return self.total_financial_metric_map.get("growth_per_share_ebitda", [])
+
 
     def get_growth_per_share_eps_last_four_years(self):
         return self.total_financial_metric_map.get("growth_per_share_eps", [])
