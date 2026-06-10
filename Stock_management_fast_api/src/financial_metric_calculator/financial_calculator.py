ce8d34b1b24421d0a0406c1f6c26f4b58c0ed207
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-06-10 11:40:25 +0200

    add dynamic cal method of devide 2 things

--- Stock_management_fast_api/src/financial_metric_calculator/financial_calculator.py
+++ Stock_management_fast_api/src/financial_metric_calculator/financial_calculator.py
@@ -42,7 +42,9 @@ class FinancialMetricCalculator:
             "capex_to_operating_cash_flow": partial(self.get_financial_metric_by_calculate_to_raw_date,
                                                                  self.total_financial_metric_map.get("Capital Expenditure", []),
                                                                  self.total_financial_metric_map.get("Operating Cash Flow", [])),
-            "capex_to_operating_income": self.get_capex_to_operating_income_last_four_years,
+            "capex_to_operating_income": partial(self.get_financial_metric_by_calculate_to_raw_date,
+                                                                 self.total_financial_metric_map.get("Capital Expenditure", []),
+                                                                 self.total_financial_metric_map.get("Operating Income", [])),
             "capex_to_revenue": self.get_capex_to_revenue_last_four_years,
             "debt_to_asset":self.get_debt_to_asset_last_four_years_last_four_years,
             "debt_to_equity":self.get_debt_to_equity_last_four_years_last_four_years,
@@ -202,17 +204,7 @@ class FinancialMetricCalculator:
 
 
 
-    def get_capex_to_operating_income_last_four_years(self):
 
-        capex_last_four_years = self.total_financial_metric_map.get("Capital Expenditure", [])
-        operating_income_last_four_years = self.total_financial_metric_map.get("Operating Income", [])
-
-        capex_to_operating_income_last_four_years = []
-
-        for capex, revenue in zip(capex_last_four_years, operating_income_last_four_years):
-            capex_to_operating_income_last_four_years.append(round(float(capex) / float(revenue), 2))
-
-        return capex_to_operating_income_last_four_years
 
     def get_capex_to_revenue_last_four_years(self):
         capex_last_four_years = self.total_financial_metric_map.get("Capital Expenditure", [])
