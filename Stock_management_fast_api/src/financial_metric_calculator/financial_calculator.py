359546c3c5003299a134d464275cf370665dc102
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-06-10 13:29:36 +0200

    add dynamic cal method of devide 2 things

--- Stock_management_fast_api/src/financial_metric_calculator/financial_calculator.py
+++ Stock_management_fast_api/src/financial_metric_calculator/financial_calculator.py
@@ -139,7 +139,7 @@ class FinancialMetricCalculator:
                                                                  self.total_financial_metric_map.get("Total Assets", [])),
             "cash_per_share": partial(self.get_financial_metric_direct_by_map, "cash_per_share"),
             "ebitda_per_share": partial(self.get_financial_metric_direct_by_map, "ebitda_per_share"),
-            "enterprise_value_to_ebit": self.get_enterprise_value_to_ebit_last_four_years,
+            "enterprise_value_to_ebit": partial(self.get_financial_metric_direct_by_map, "enterprise_value_to_ebit"),
             "enterprise_value_to_ebitda": self.get_enterprise_value_to_ebitda_last_four_years,
             "enterprise_value_to_fcf": self.get_enterprise_value_to_fcf_last_four_years,
             "enterprise_value_to_ocf": self.get_enterprise_value_to_ocf_last_four_years,
@@ -311,8 +311,7 @@ class FinancialMetricCalculator:
             revenue_per_employee_last_four_years.append(round(float(revenue) / float(employee_amount), 2))
         return revenue_per_employee_last_four_years
 
-    def get_enterprise_value_to_ebit_last_four_years(self):
-        return self.total_financial_metric_map.get("enterprise_value_to_ebit", [])
+
 
     def get_enterprise_value_to_ebitda_last_four_years(self):
         return self.total_financial_metric_map.get("enterprise_value_to_ebitda", [])
