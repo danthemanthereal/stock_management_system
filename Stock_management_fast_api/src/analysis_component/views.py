202aa969e3e2e3e00e0acfc94cdcbfea36c0a157
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-06-20 23:12:52 +0200

    improve rename error

--- Stock_management_fast_api/src/analysis_component/views.py
+++ Stock_management_fast_api/src/analysis_component/views.py
@@ -1,7 +1,7 @@
 import traceback
 from typing import Optional
 from uuid import UUID
-from fastapi import APIRouter, Request, Depends,Form, Query
+from fastapi import APIRouter, Request, Depends, Form, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.analysis_component.schema import WikiUpdate
@@ -19,12 +19,14 @@ templates = Jinja2Templates(directory="templates")
 
 analysis_router = APIRouter(prefix="/analysis", tags=["analysis"])
 
+
 @analysis_router.get("/")
 def analysis(request: Request):
     return templates.TemplateResponse(
         request=request,
         name="analysis/analysis.html",
         context={"request": request})
+
 
 @analysis_router.post("/get-summary-url", response_class=HTMLResponse)
 async def analyze_url(request: Request,
@@ -50,6 +52,7 @@ async def analyze_url(request: Request,
             context={"request": request},
         )
 
+
 @analysis_router.post("/get-summary-by-yt-video", response_class=HTMLResponse)
 async def get_yt_transcript(request: Request,
                             url: str = Form(...),
@@ -65,7 +68,7 @@ async def get_yt_transcript(request: Request,
             context={
                 "request": request,
                 "companies": companies_array,
-                "yt_url":url
+                "yt_url": url
             }
         )
     except Exception as e:
@@ -76,6 +79,7 @@ async def get_yt_transcript(request: Request,
             name="error.html",
             context={"request": request},
         )
+
 
 @analysis_router.api_route("/show-saved-financial-metrics", methods=["GET", "POST"], response_class=HTMLResponse)
 async def show_saved_financial_metrics_page(
@@ -89,11 +93,12 @@ async def show_saved_financial_metrics_page(
 
         return await analysis_service.get_current_start_page(
             request=request,
-        current_user_id=current_user_id)
+            current_user_id=current_user_id)
     except Exception as e:
         print(f"Error: {e}")
         traceback.print_exc()
         return templates.TemplateResponse(request=request, name="error.html", context={"request": request})
+
 
 @analysis_router.post("/add-metric-to-current-template", response_class=HTMLResponse)
 async def add_to_current_selected_template_new_financial_metric(
@@ -124,6 +129,7 @@ async def add_to_current_selected_template_new_financial_metric(
         traceback.print_exc()
         return templates.TemplateResponse(request=request, name="error.html", context={"request": request})
 
+
 @analysis_router.post("/create-new-template-with-current-properties", response_class=HTMLResponse)
 async def create_new_template_of_current_financial_metrics_properties(
         request: Request,
@@ -150,6 +156,7 @@ async def create_new_template_of_current_financial_metrics_properties(
         traceback.print_exc()
         return templates.TemplateResponse(request=request, name="error.html", context={"request": request})
 
+
 @analysis_router.post("/change-selected-template")
 async def change_selected_template(
         request: Request,
@@ -175,6 +182,7 @@ async def change_selected_template(
         traceback.print_exc()
         return templates.TemplateResponse(request=request, name="error.html", context={"request": request})
 
+
 @analysis_router.get("/edit-metric-of-current-template/{last_selected_branch_profile_id}/{metric_id}")
 async def show_edit_financial_metric_of_current_template(
         request: Request,
@@ -183,7 +191,6 @@ async def show_edit_financial_metric_of_current_template(
         db: AsyncSession = Depends(get_db),
         current_user_id: UUID = Depends(get_current_user_id)
 ):
-
     analysis_service = AnalysisService(db)
 
     template = await analysis_service.get_template_by_id(template_id=last_selected_branch_profile_id)
@@ -208,6 +215,7 @@ async def show_edit_financial_metric_of_current_template(
             "metric_categories": metric_categories,
             "selected_branch_profile_id": last_selected_branch_profile_id,
         })
+
 
 @analysis_router.post("/update-metric-of-current-template-config/{config_id}")
 async def update_metric_of_current_template(
@@ -241,6 +249,7 @@ async def update_metric_of_current_template(
         traceback.print_exc()
         return templates.TemplateResponse(request=request, name="error.html", context={"request": request})
 
+
 @analysis_router.post("/delete-selected-metrics-for-this-template")
 async def delete_selected_metrics_for_this_template(
         request: Request,
@@ -268,6 +277,7 @@ async def delete_selected_metrics_for_this_template(
         traceback.print_exc()
         return templates.TemplateResponse(request=request, name="error.html", context={"request": request})
 
+
 @analysis_router.post("/find-potential-stocks", response_class=HTMLResponse)
 def find_potential_stocks_page(request: Request):
     try:
@@ -284,18 +294,21 @@ def find_potential_stocks_page(request: Request):
             context={"request": request}
         )
 
+
 @analysis_router.post("/find-candidates")
 async def find_potential_stocks(filters: dict,
-                          db: AsyncSession = Depends(get_db),):
+                                db: AsyncSession = Depends(get_db), ):
     analysis_service = AnalysisService(db)
 
     return await analysis_service.find_potential_stock_of_filter(filters)
 
+
 @analysis_router.get("/get-financial-metrics", response_class=HTMLResponse)
 async def get_evaluation_of_financial_metrics_of_current_user_last_selected_template(request: Request,
                                                                                      company: str,
-                                                  db: AsyncSession = Depends(get_db),
-                                                  current_user_id: UUID = Depends(get_current_user_id)):
+                                                                                     db: AsyncSession = Depends(get_db),
+                                                                                     current_user_id: UUID = Depends(
+                                                                                         get_current_user_id)):
     try:
 
         analysis_service = AnalysisService(db)
@@ -315,10 +328,10 @@ async def get_evaluation_of_financial_metrics_of_current_user_last_selected_temp
             context={"request": request}
         )
 
+
 @analysis_router.get("/get-news")
 async def get_news_of_stock_with_finnhub(request: Request, stock: str = Query(...),
-                                   db: AsyncSession = Depends(get_db)):
-
+                                         db: AsyncSession = Depends(get_db)):
     analysis_service = AnalysisService(db)
 
     headline_url = await analysis_service.get_headline_url_dict(stock)
@@ -331,9 +344,10 @@ async def get_news_of_stock_with_finnhub(request: Request, stock: str = Query(..
         }
     )
 
+
 @analysis_router.get("/get-stock-market-news")
 async def get_stock_market_news(request: Request,
-                          db: AsyncSession = Depends(get_db)):
+                                db: AsyncSession = Depends(get_db)):
     try:
         analysis_service = AnalysisService(db)
 
@@ -354,10 +368,11 @@ async def get_stock_market_news(request: Request,
             name="error.html",
         )
 
+
 @analysis_router.get("/get-stock-market-wiki-page")
 async def get_stock_market_wiki_page(request: Request,
-                               db: AsyncSession = Depends(get_db)
-                               ):
+                                     db: AsyncSession = Depends(get_db)
+                                     ):
     try:
         analysis_service = AnalysisService(db)
 
@@ -378,11 +393,12 @@ async def get_stock_market_wiki_page(request: Request,
             name="error.html",
         )
 
+
 @analysis_router.post("/update-stock-wiki", response_class=HTMLResponse)
 async def update_stock_wiki(
-    request: Request,
-    data: WikiUpdate,
-    db: AsyncSession = Depends(get_db)
+        request: Request,
+        data: WikiUpdate,
+        db: AsyncSession = Depends(get_db)
 ):
     analysis_service = AnalysisService(db)
 
@@ -401,10 +417,11 @@ async def update_stock_wiki(
         }
     )
 
+
 @analysis_router.get("/industry-wiki-page", response_class=HTMLResponse)
 async def get_industry_wiki_page(request: Request,
-                           db: AsyncSession = Depends(get_db),
-                           current_user_id: UUID = Depends(get_current_user_id)):
+                                 db: AsyncSession = Depends(get_db),
+                                 current_user_id: UUID = Depends(get_current_user_id)):
     try:
         analysis_service = AnalysisService(db)
 
@@ -429,6 +446,7 @@ async def get_industry_wiki_page(request: Request,
             name="error.html",
         )
 
+
 @analysis_router.get("/industry-edit-wiki-page")
 async def get_industry_edit_wiki_page(request: Request,
                                       db: AsyncSession = Depends(get_db),
@@ -439,7 +457,8 @@ async def get_industry_edit_wiki_page(request: Request,
 
         wiki_pages = await analysis_service.get_industry_wiki_pages_of_current_user(current_user_id)
 
-        created_industries_of_current_user = await analysis_service.get_all_created_industries_of_current_user(current_user_id)
+        created_industries_of_current_user = await analysis_service.get_all_created_industries_of_current_user(
+            current_user_id)
 
         return templates.TemplateResponse(
             request=request,
@@ -464,15 +483,18 @@ async def get_industry_edit_wiki_page(request: Request,
 
 @analysis_router.post("/add-to-current-user-new-industry")
 async def add_to_current_user_new_industry(request: Request,
-                                            user_input: str = Form(...),
+                                           industry_name: str = Form(...),
                                            db: AsyncSession = Depends(get_db),
                                            current_user_id: UUID = Depends(get_current_user_id)
-                                      ):
+                                           ):
     try:
         analysis_service = AnalysisService(db)
-        await analysis_service.add_to_current_user_new_industry(user_input, current_user_id)
+        await analysis_service.add_to_current_user_new_industry(industry_name, current_user_id)
 
         wiki_pages = await analysis_service.get_industry_wiki_pages_of_current_user(current_user_id)
+
+        created_industries_of_current_user = await analysis_service.get_all_created_industries_of_current_user(
+            current_user_id)
 
         return templates.TemplateResponse(
             request=request,
@@ -481,8 +503,8 @@ async def add_to_current_user_new_industry(request: Request,
             context=
             {
                 "request": request,
-                "wiki_pages": wiki_pages
-
+                "wiki_pages": wiki_pages,
+                "created_industries_of_current_user": created_industries_of_current_user
             }
         )
 
@@ -493,3 +515,49 @@ async def add_to_current_user_new_industry(request: Request,
             request=request,
             name="error.html",
         )
+
+
+@analysis_router.post("/update-wiki-page-current-selected-industry")
+async def update_wiki_page_current_selected(request: Request,
+                                            db: AsyncSession = Depends(get_db),
+                                            current_user_id: UUID = Depends(get_current_user_id),
+                                            selected_industry: str = Form(...),
+                                            industry_name: str = Form(...),
+                                            action: str = Form(...)
+                                            ):
+    try:
+        analysis_service = AnalysisService(db)
+
+        print("selected_industry", selected_industry)
+
+        print("industry_name", industry_name)
+
+        print("action", action)
+
+        wiki_pages = await analysis_service.get_industry_wiki_pages_of_current_user(current_user_id)
+
+        created_industries_of_current_user = await analysis_service.get_all_created_industries_of_current_user(
+            current_user_id)
+
+        return templates.TemplateResponse(
+            request=request,
+
+            name="analysis/edit_industry_of_current_user.html",
+            context=
+            {
+                "request": request,
+                "wiki_pages": wiki_pages,
+                "created_industries_of_current_user": created_industries_of_current_user
+            }
+        )
+
+
+
+    except Exception as e:
+        print(f"Error: {e}")
+        traceback.print_exc()
+        return templates.TemplateResponse(
+            request=request,
+            name="error.html",
+        )
+
