0927d5ff82db0f014dcd011c6486f9e0d4dbf6f2
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-06-20 12:47:29 +0200

    add a helper method for converting into dict if necessary

--- Stock_management_fast_api/src/ai_financial_metricevaluation_component/utils.py
+++ Stock_management_fast_api/src/ai_financial_metricevaluation_component/utils.py
@@ -1,3 +1,6 @@
+from typing import Union, List, Tuple, Any
+
+
 def get_all_used_categories_for_eval(category_satisfied_map: dict[str, list])->list[str]:
     if isinstance(category_satisfied_map, dict):
         return list(category_satisfied_map.keys())
@@ -21,11 +24,24 @@ def get_each_metrics_list_by_category(
                                                                                  list[str],
                                                                                  list[str],
                                                                                  list[str]):
+
+    print("get a cat map ")
+    print(category_satisfied_map)
     return (
-        category_satisfied_map.get(category, []),
-        category_unsatisfied_map.get(category, []),
-        category_satisfied_only_reference_value_map.get(category, []),
-        category_unsatisfied_only_reference_value_map.get(category, []),
-        category_satisfied_only_development_map.get(category, []),
-        category_unsatisfied_development_map.get(category, []),
+        convert_into_dictionary_if_necessary(category_satisfied_map).get(category, []),
+        convert_into_dictionary_if_necessary(category_unsatisfied_map).get(category, []),
+        convert_into_dictionary_if_necessary(category_satisfied_only_reference_value_map).get(category, []),
+        convert_into_dictionary_if_necessary(category_unsatisfied_only_reference_value_map).get(category, []),
+        convert_into_dictionary_if_necessary(category_satisfied_only_development_map).get(category, []),
+        convert_into_dictionary_if_necessary(category_unsatisfied_development_map).get(category, []),
     )
+
+def convert_into_dictionary_if_necessary(maybe_dict: Union[dict, List[Tuple[Any, Any]]]) -> dict:
+    if isinstance(maybe_dict, dict):
+        return maybe_dict
+    if isinstance(maybe_dict, list):
+        try:
+            return dict(maybe_dict)
+        except Exception as e:
+            raise TypeError(f"List cannot be converted to dict: {e}")
+    raise TypeError(f"Expected dict or list of tuples, got {type(maybe_dict)}")
