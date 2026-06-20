9be8be5b6180027d0b44337aef75c802247b5129
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-06-20 10:19:59 +0200

    adpat constructor at all

--- Stock_management_fast_api/ai_metric_analysis_component/capital_cost_analysis_component/capital_cost_analysis.py
+++ Stock_management_fast_api/ai_metric_analysis_component/capital_cost_analysis_component/capital_cost_analysis.py
@@ -3,8 +3,14 @@ from ai_metric_analysis_component.ai_metric_analysis import AIMetricAnalysisComp
 
 class CapitalCostAnalysis(AIMetricAnalysisComponent):
 
-    def __init__(self):
+    def __init__(self,
+                 model_name: str,
+                 api_key: str,
+                 user_prompt_path: str,
+                 system_prmpt_path: str):
         super().__init__()
+        self.model_name = model_name
+        self.api_key = api_key
 
     def analyse_financial_metrics(self, all_to_considered_financial_metrics):
         pass
--- Stock_management_fast_api/ai_metric_analysis_component/valuation_multiples_analysis_component/valuation_multiples_analysis.py
+++ Stock_management_fast_api/ai_metric_analysis_component/valuation_multiples_analysis_component/valuation_multiples_analysis.py
@@ -3,8 +3,15 @@ from ai_metric_analysis_component.ai_metric_analysis import AIMetricAnalysisComp
 
 class EvaluationMultiplesAnalysis(AIMetricAnalysisComponent):
 
-    def __init__(self):
+    def __init__(self,
+                 model_name: str,
+                 api_key: str,
+                 user_prompt_path: str,
+                 system_prmpt_path: str
+                 ):
         super().__init__()
+        self.model_name = model_name
+        self.api_key = api_key
 
     def analyse_financial_metrics(self, all_to_considered_financial_metrics):
         pass
