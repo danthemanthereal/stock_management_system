886617773ff74d9004fb909b3e0a79370b30f974
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-06-20 12:56:53 +0200

    imprave error of system prompt

--- Stock_management_fast_api/src/kaparthies_llm_wiki_component/llm_wiki.py
+++ Stock_management_fast_api/src/kaparthies_llm_wiki_component/llm_wiki.py
@@ -73,7 +73,7 @@ class LLMWiki:
         response = client.chat.completions.create(
             model=self.groq_model_name,
             messages=[
-                {"role": "system_prompt.txt",
+                {"role": "system",
                  "content": system_prompt
                  },
                 {
@@ -97,7 +97,7 @@ class LLMWiki:
         response = client.chat.completions.create(
             model=self.groq_model_name,
             messages=[
-                {"role": "system_prompt.txt",
+                {"role": "system",
                  "content": system_prompt
                  },
                 {
@@ -122,7 +122,7 @@ class LLMWiki:
         response = client.chat.completions.create(
             model=self.groq_model_name,
             messages=[
-                {"role": "system_prompt.txt",
+                {"role": "system",
                  "content": system_prompt
                  },
                 {
@@ -147,7 +147,7 @@ class LLMWiki:
         response = client.chat.completions.create(
             model=self.groq_model_name,
             messages=[
-                {"role": "system_prompt.txt",
+                {"role": "system",
                  "content": system_prompt
                  },
                 {
--- Stock_management_fast_api/src/stock_market_artikel_analysis_component/stock_market_analysis.py
+++ Stock_management_fast_api/src/stock_market_artikel_analysis_component/stock_market_analysis.py
@@ -33,7 +33,7 @@ class StockMarketAnalysis:
             response = client.chat.completions.create(
                 model=self.model_name,
                 messages=[
-                    {"role": "system_prompt.txt",
+                    {"role": "system",
                      "content": system_prompt
                      },
                     {
