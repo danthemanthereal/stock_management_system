1727dd813ed3efcea3bb313b6ef24070c5b2d1be
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-07-30 15:33:54 +0200

    save state

--- Stock_management_fast_api/src/analysis_component/service.py
+++ Stock_management_fast_api/src/analysis_component/service.py
@@ -494,6 +494,7 @@ class AnalysisService:
                 new_content=file_content_str,
                 industry_name=industry_name,
             )
+            
 
             new_combined_bear_factors = await llm_wiki_component.ingest_bear_factors_wiki_page(
                 industry_name=industry_name,
@@ -520,9 +521,6 @@ class AnalysisService:
                 industry_name=industry_name,
                 new_wiki_page=updated_page_industry_page
             )
-
-            print("new combind bear factors: ", new_combined_bear_factors)
-            print("new combind bull factors: ", new_combined_bull_factors)
 
             await industry_service.update_bear_and_bull_of_selected_industry_of_current_user(
                 current_user_id=current_user_id,
